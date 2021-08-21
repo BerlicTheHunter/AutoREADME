066278917156176efa4d0d5f0f9ae8120ccda47c
Berlic <bsorrell3@gmail.com>
2021-08-20 22:23:15 -0400

    added in the markdown for readme as template literal

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -8,12 +8,58 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` 
+# ${data.title}
 
+![License Badge](https://img.shields.io/badge/License-MIT-green)
+
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+  * [License](#license) 
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](Questions)
+
+----
+
+## Description
+${data.description}
+
+----
+
+## Installation
+${data.installation}
+
+----
+
+## Usage
+${data.usage}
+
+  ### -Lisence
+  ### ${data.lisence}
+
+----
+
+## Contributing
+${data.contributing}
+
+----
+
+## Tests
+${data.tests}
+
+----
+
+## Questions
+${data.questions}
 `;
 }
 
